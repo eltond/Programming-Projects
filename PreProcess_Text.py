d2c649af8ab4367c75b66d4ab08adb936dcc314a
eltond <eltond@uw.edu>
2019-05-02 00:34:39 -0700

    Update PreProcess_Text.py
    - Localized word -> sentence search
    - Fixed global list variables

--- PreProcess_Text.py
+++ PreProcess_Text.py
@@ -19,24 +19,12 @@ stpwords.extend(newStopWords)
 
 translator = str.maketrans('','', string.punctuation)
 
-wrd=[]
-synsets = []
-defn = []
-full_sent = []
 hyprnympaths = []
-expand_tokens = []
-expand_Lemms = []
 wnsynsets = []
+row_flag = []
 
-def flattenfile(f):
-    flattened_list = []
-    for x in f:
-        flattened_list.append(x)
-    return flattened_list
-
-#Text = ['I really really love love love artificial intelligence and robotics']
 startTime = datetime.now()
-connection_string="Driver={ODBC Driver 17 for SQL Server};Server=,14330;Database=;Trusted_Connection=Yes"
+connection_string="Driver={ODBC Driver 17 for SQL Server};Server=MRICZDADSAPD005,14330;Database=TextAnalytics;Trusted_Connection=Yes"
 query="""select top 108000 a.transcriptComponentId,   a.componenttext
 from dbo.FactEarningsText a
 
@@ -54,19 +42,14 @@ df = rx_data_step(ds)
 dfToList = df['componenttext'].tolist()
     
 #conn = pyodbc.connect('driver={ODBC Driver 11 for SQL Server};'
-#                      'server=;'
-#                      'database=;'
+#                      'server=MRICZDADSAPD005;'
+#                      'database=TextAnalytics;'
 #                      'Trusted_Connection=yes;')
 
-#sql = "select top 5 * from dbo.FactEarningsText a"
+#sql = "select top 1000 * from dbo.FactEarningsText a"
     
 #data = pd.read_sql(sql,conn)
 #dfToList = data['componenttext'].tolist()
-
-
-FileCompressed = flattenfile(dfToList)
-sent_text = nltk.sent_tokenize(' '.join(FileCompressed))
-
 
 class CustomVectorizer(CountVectorizer):
                     
@@ -76,7 +59,7 @@ class CustomVectorizer(CountVectorizer):
                 text = f.lower()
                 text_contraction = cont.fix(text)
                 text_punc = text_contraction.translate(translator)
-                text_clean = ' '.join([word for word in text_punc.split() if(len(word.lower())>1 and word.lower() not in stpwords)])
+                text_clean = ' '.join([word for word in text_punc.split() if(len(word.lower())>1)])
                 text_sent = ' '.join([i for i in text_clean.split() if(i.isalnum() and not i.isdigit())])
                 return text_sent
         return (Text_Cleanup)   
@@ -92,58 +75,61 @@ class CustomVectorizer(CountVectorizer):
     def build_analyzer(self):
         
         # load stop words using CountVectorizer's built in method
+        stop_words = self.get_stop_words()
         preprocess = self.build_preprocessor()
         Tokenize = self.build_tokenizer()
         # create the analyzer that will be returned by this method
         def analyser(doc):
-            
+            expand_tokens = []
+            expand_Lemms = []
+            synsets = []
             # apply the preprocessing and tokenzation steps
+            sent_text = nltk.sent_tokenize(doc)
             doc_clean = preprocess(doc)
             tokens = Tokenize(doc_clean)
-            #tokens = lemmatizer(doc_clean)
-            lemmatized_tokens = [token for token in tokens]
             # use CountVectorizer's _word_ngrams built in method
             # to remove stop words and extract n-grams        
-            n_grams = self._word_ngrams(lemmatized_tokens)
+            n_grams = list(set(self._word_ngrams(tokens,stop_words)))
             for x in n_grams:
                 my_regex = r"\b(?=\w)" + re.escape(x) + r"\b(?!\w)"
                 matched_sent = [s for s in sent_text if len(re.findall(my_regex, s, re.IGNORECASE)) > 0]
                 expand_tokens.append(x)
                 expand_Lemms.append(matched_sent)
             Expanded_Token_Lemmd = pd.DataFrame({'Word': expand_tokens,'Sentences': expand_Lemms})
-            for i in range(0, len(Expanded_Token_Lemmd)):
+            for i in range(0, len(Expanded_Token_Lemmd.index)):
                 for row_len in range(0, len(Expanded_Token_Lemmd.iloc[i]['Sentences'])):
                     x = lesk(Expanded_Token_Lemmd.iloc[i]['Sentences'][row_len],Expanded_Token_Lemmd.iloc[i]['Word'].lower().replace(' ','_'))
                     if not x:
-                        synsets.append("No Synset")
+                        synsets.append(Expanded_Token_Lemmd.iloc[i]['Word'].lower())
                     else:
                         synsets.append(str(x)[8:-2])
             return(synsets)
         return(analyser)
 
-
-custom_vec = CustomVectorizer(ngram_range=(1,2),min_df=8,stop_words=stpwords)
+custom_vec = CustomVectorizer(ngram_range=(1,3),min_df=0.05,stop_words=stpwords)
 
 custom_vec.fit(dfToList)
 
 v1 = custom_vec.vocabulary_
 Vocab =  pd.DataFrame.from_dict(v1,orient = "index")
-DimVocab = Vocab.reset_index().rename(columns = {0:"VocabKey","index":"Word"})
+DimVocab = Vocab.reset_index().rename(columns = {0:"VocabKey","index":"Synset"})
 
 for i in range(0, len(DimVocab)):
     try:
-        syn = wn.synset(DimVocab.iloc[i]['Word'])
+        syn = wn.synset(DimVocab.iloc[i]['Synset'])
         for paths in syn.hypernym_paths():
             wnsynsets.append(syn)
-            hyprnympaths.append(paths)    
+            hyprnympaths.append(paths)
+            row_flag.append("Synset")
     except:
-        wnsynsets.append("No Synset")
+        wnsynsets.append(DimVocab.iloc[i]['Synset'])
         hyprnympaths.append("No Hypernym Path")
-DimVocabFinal = pd.DataFrame({'Synsets': wnsynsets,'Hypernym Path':hyprnympaths})
-DimVocabFinal['VocabKey'] = range(1, 1+len(DimVocabFinal))
+        row_flag.append("n-grams")
+DimVocabFinal = pd.DataFrame({'Synsets': wnsynsets,'Hypernym Path':hyprnympaths, 'Keyword Flag':row_flag})
 
 dsout = RxSqlServerData(table = "UtilVocabularyNew", connection_string = connection_string,rows_per_read=10000)
 rx_data_step(DimVocabFinal,dsout,overwrite=True)
+
 
 print(datetime.now() - startTime)
 
